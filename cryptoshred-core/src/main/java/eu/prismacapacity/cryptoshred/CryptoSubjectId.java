e3367edb0db09621d2f846670496c591ad8842a9
Uwe Schaefer <uwe.schaefer@mercateo.com>
2020-02-18 20:55:49 +0100

    build trigger

--- cryptoshred-core/src/main/java/eu/prismacapacity/cryptoshred/CryptoSubjectId.java
+++ cryptoshred-core/src/main/java/eu/prismacapacity/cryptoshred/CryptoSubjectId.java
@@ -2,9 +2,11 @@ package eu.prismacapacity.cryptoshred;
 
 import java.util.UUID;
 
+import lombok.NonNull;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
 public class CryptoSubjectId {
+    @NonNull
 	final UUID id;
 }
