249123873ab670bdd55a95b9e698c4b34f2200c2
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2020-02-19 17:39:59 +0100

    minor refactorings

--- cryptoshred-cloud-aws/src/main/java/eu/prismacapacity/cryptoshred/cloud/aws/Maps.java
+++ cryptoshred-cloud-aws/src/main/java/eu/prismacapacity/cryptoshred/cloud/aws/Maps.java
@@ -2,7 +2,7 @@ package eu.prismacapacity.cryptoshred.cloud.aws;
 
 import java.util.HashMap;
 
-public class Maps {
+class Maps {
 
 	public static <K, V> HashMap<K, V> of(K k, V v) {
 		HashMap<K, V> ret = new HashMap<K, V>();
