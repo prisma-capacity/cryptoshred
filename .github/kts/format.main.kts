d57a03158aa215833a7f17ef1d862a5811f0715c
Uwe Schaefer <uwe.schaefer@prisma-capacity.eu>
2022-06-26 16:46:15 +0200

    use-kotlin-dsl-for-ghactions

---
+++ .github/kts/format.main.kts
@@ -0,0 +1,55 @@
+#!/usr/bin/env kotlin
+
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.20.0")
+
+import it.krzeminski.githubactions.actions.CustomAction
+import it.krzeminski.githubactions.actions.actions.CheckoutV3
+import it.krzeminski.githubactions.actions.actions.SetupJavaV3
+import it.krzeminski.githubactions.domain.RunnerType
+import it.krzeminski.githubactions.domain.Workflow
+import it.krzeminski.githubactions.domain.triggers.Push
+import it.krzeminski.githubactions.dsl.workflow
+import it.krzeminski.githubactions.yaml.writeToFile
+import java.nio.file.Paths
+
+public val workflowFormat: Workflow = workflow(
+      name = "Format",
+      on = listOf(
+        Push(),
+        ),
+      sourceFile = Paths.get(".github/kts/format.main.kts"),
+    ) {
+      job(
+        id = "formatting",
+        runsOn = RunnerType.UbuntuLatest,
+      ) {
+        uses(
+          name = "Checkout",
+          action = CheckoutV3(
+            fetchDepth = CheckoutV3.FetchDepth.Infinite,
+          ),
+        )
+        uses(
+          name = "SetupJava",
+          action = SetupJavaV3(
+            javaVersion = "11",
+            distribution = SetupJavaV3.Distribution.Corretto,
+          ),
+        )
+        uses(
+          name = "GooglejavaformatActionV3",
+          action = CustomAction(
+            actionOwner = "axel-op",
+            actionName = "googlejavaformat-action",
+            actionVersion = "v3",
+            inputs = mapOf(
+              "args" to " --replace",
+              "version" to "1.7",
+            )
+          ),
+        )
+      }
+
+    }
+
+workflowFormat.writeToFile()
